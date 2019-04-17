bbbd31dfceaea5b6bb4d9f8d4827126828d942e5
Kip DeCastro <kip@Kips-MacBook-Pro-2.local>
2019-04-17 16:56:47 -0600

    fixed openjre version

--- install-jenkins-ubuntu-18.sh
+++ install-jenkins-ubuntu-18.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt update
 echo 'Installing Java 8'
-sudo apt install openjdk-8-jdk openjdk-9-jre
+sudo apt install openjdk-8-jdk openjdk-8-jre
 wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt update
