d5dc86b1e8eb4aaebc65b2b30c2e05eba7bf4bf5
Kip DeCastro <kip@Kips-MacBook-Pro-2.local>
2019-04-18 12:27:51 -0600

    Added yes pipes

--- install-docker-ubuntu-18.sh
+++ install-docker-ubuntu-18.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update
-sudo apt install docker.io
+yes | sudo apt install docker.io
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo usermod -aG docker jenkins
--- install-jenkins-ubuntu-18.sh
+++ install-jenkins-ubuntu-18.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 sudo apt update
 echo 'Installing Java 8'
-sudo apt install openjdk-8-jdk openjdk-8-jre
+yes | sudo apt install openjdk-8-jdk openjdk-8-jre
 wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt update
 echo 'Installing Jenkins'
-sudo apt install jenkins
+yes | sudo apt install jenkins
 sudo systemctl start jenkins
 sudo systemctl enable jenkins
-cat /var/lib/jenkins/secrets/initialAdminPassword
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
