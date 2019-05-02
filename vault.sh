eeef740b26bf7426dca241232ae37d2d54edc22c
Kip DeCastro <kipdec@gmail.com>
2019-05-02 15:42:31 -0600

    Installing in a more sensible manner

--- vault.sh
+++ vault.sh
@@ -1,13 +1,18 @@
 // Install unzip
+echo "Installing unzip..."
 sudo apt install unzip
 
 // Get vault
+echo "Getting vault..."
 wget https://releases.hashicorp.com/vault/1.1.2/vault_1.1.2_linux_amd64.zip
 
 // Unzip it
+echo "Unzipping..."
 unzip vault_1.1.2_linux_amd64.zip
 
-mkdir .vault
-mv vault .vault
+echo "Changing ownership..."
+sudo chown root:root vault
 
-echo "export PATH=~/.vault:$PATH"  >> ~/.bashrc
+echo "Moving directories..."
+sudo mv vault /usr/local/bin
+vault --version
