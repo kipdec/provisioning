8b578aa5d9326dd263b1fb234ff4f7b08d077047
Kip DeCastro <kipdec@gmail.com>
2019-05-02 15:52:28 -0600

    Added vault service file

--- vault.sh
+++ vault.sh
@@ -22,4 +22,14 @@ echo "Cleaning up..."
 echo "Removing the .zip..."
 sudo rm vault_1.1.2_linux_amd64.zip
 
+echo "Setting up vault..."
+vault -autocomplete-install
+complete -C /usr/local/bin/vault vault
+sudo setcap cap_ipc_lock=+ep /usr/local/bin/vault
+
+echo "Adding non-priveleged user to run vault"
+sudo useradd --system --home /etc/vault.d --shell /bin/false vault
+
+echo "Copying vault service file..."
+sudo cp vault.service /etc/systemd/system/vault.service
 echo "Done!"
